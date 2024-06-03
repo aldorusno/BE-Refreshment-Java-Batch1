45a4084f26c0210067b040b4a70033da308baa1e
johan <johanwirasugianto@gmail.com>
2024-06-03 12:33:52 +0700

    Initial Project Java

---
+++ src/main/java/com/metrodata/initialize_project/InitializeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.metrodata.initialize_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metrodata/initialize_project/InitializeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrodata.initialize_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
